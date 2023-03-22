86853b15845dccb044a68fb89a1e9c858a1ecf6b
tranmuse <tranmuse@gmail.com>
2023-03-22 11:25:15 -0700

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="p:domain_verify"
+          content="32419f794cd4915f39a0179cdf9f0846"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
